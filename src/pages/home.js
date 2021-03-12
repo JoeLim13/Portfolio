07afb2e036acee4e39267316f697879b9723dc2c
Joe Lim <joecounterzero@hotmail.com>
2021-03-13 01:30:32 +0800

    edited homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,27 +1,17 @@
 import React from "react";
 
 //Styles
-import "../App.scss";
+import "../scss/Home.scss";
 
 function home() {
   return (
-    <div className="container">
-      <div className="wrapper">
-        <div className="home">
-          <h5>
-            <b>HELLO</b>, welcome to my website. My name is Lim Ean Han. You can
-            call me Joe if you like. Head over to <b>menu</b> and check out my
-            portfolio. If you wish to head over to my social media(s), feel free
-            to click the icons on top.
-            <br></br>I will constantly update my website to make it more
-            interactive for everyone of you. Currently, the website is not
-            friendly to mobile user. I will update in the future, I promise.
-            Last but not least, whenever a project is created by me, I will
-            share it here. Stay tuned for more updates!
-          </h5>
-        </div>
-      </div>
-    </div>
+    <section>
+      <video src="homevid.MOV" autoPlay loop muted></video>
+      <h2>
+        Welcome to My Website<br></br>
+        <span>click on menu</span>
+      </h2>
+    </section>
   );
 }
 
